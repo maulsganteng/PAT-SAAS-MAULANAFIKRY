ab2536e61f7349ec78f3f12bfc178f4c96d0fef5
Fikry <muhammadfikri6910@smk.belajar.id>
2024-06-06 09:07:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './header/header';
+import Hero from './konten/konten';
 
 function App() {
   return (
--- src/konten/konten.js
+++ src/konten/konten.js
@@ -1,1 +1,8 @@
 import React from "react";
+
+// function Hero() {
+//     return (
+//     j
+//     )
+// }
+// export default Hero;
